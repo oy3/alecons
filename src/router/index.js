2260a081e7bc26c2a9e9fbb729a1f21620edcb8f
Temitope Oyeyinka <topeisco@gmail.com>
2025-08-08 06:01:30 +0100

    router scrollBehavior

--- src/router/index.js
+++ src/router/index.js
@@ -7,41 +7,44 @@ import Admissions from "../views/Admissions.vue";
 import Faculty from "../views/Faculty.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About,
-    },
-    {
-        path: "/programs",
-        name: "Programs",
-        component: Programs,
-    },
-    {
-        path: "/admissions",
-        name: "Admissions",
-        component: Admissions,
-    },
-    {
-        path: "/faculty",
-        name: "Faculty",
-        component: Faculty,
-    },
-    {
-        path: "/contact",
-        name: "Contact",
-        component: Contact,
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/programs",
+    name: "Programs",
+    component: Programs,
+  },
+  {
+    path: "/admissions",
+    name: "Admissions",
+    component: Admissions,
+  },
+  {
+    path: "/faculty",
+    name: "Faculty",
+    component: Faculty,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
 });
 
 export default router;
